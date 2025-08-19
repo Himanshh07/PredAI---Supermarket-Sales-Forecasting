4c78deb27e7cb5289c4519333ecd24ef1cf2cfe7
Himanshu <152769233+Himanshh07@users.noreply.github.com>
2025-08-20 00:32:10 +0530

    Update main.py

--- Sale Forecasting/main.py
+++ Sale Forecasting/main.py
@@ -58,5 +58,9 @@ def predict():
     return render_template('index.html', prediction=prediction, error=error)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT
+    app.run(host="0.0.0.0", port=port, debug=False)
+
